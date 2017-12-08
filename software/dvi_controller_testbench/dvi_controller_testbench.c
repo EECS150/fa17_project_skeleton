39dbbf0698a992009042fd8429fcacb85043e837
Vighnesh Iyer <vighnesh.iyer@berkeley.edu>
2017-12-07 17:43:32 -0800

    fixed off-by-one bug in dvi_controller_testbench.c software

--- software/dvi_controller_testbench/dvi_controller_testbench.c
+++ software/dvi_controller_testbench/dvi_controller_testbench.c
@@ -3,7 +3,7 @@
 
 int main(void) {
     // You should test both the fill and swline in simulation
-    swline(1, 0, 0, 1024, 768);    
+    swline(1, 0, 0, 1023, 767);    
     //fill(1);
     while (1) {}
     return 0;
